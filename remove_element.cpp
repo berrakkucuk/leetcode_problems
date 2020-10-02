5010d9d6e8d2eb6b5a9a08be6780cd99493fc52f
Berrak <46188572+berrakkucuk@users.noreply.github.com>
2020-10-02 12:47:15 +0300

    Remove Element C++ (#27)
    Problem: Given an array nums and a value val, remove all instances of that value in-place and return the new length.
    
    Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
    
    The order of elements can be changed. It doesn't matter what you leave beyond the new length.
    
    Example 1:
    
    Given nums = [3,2,2,3], val = 3,
    
    Your function should return length = 2, with the first two elements of nums being 2.
    
    It doesn't matter what you leave beyond the returned length.

---
+++ remove_element.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        
+        int len = nums.size();
+        
+        for(int i=0; i<len; i++){
+            
+            if( nums[i] == val ) {
+                
+               nums.erase(nums.begin()+i);
+                len--;
+                i--;
+            }
+            
+        }
+        
+      
+        
+        return len;
+        
+    }
+};
