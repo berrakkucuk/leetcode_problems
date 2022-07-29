44addd07efc810af5160a2d4d45716a71f4cc041
Berrak <46188572+berrakkucuk@users.noreply.github.com>
2022-07-29 10:10:37 +0300

    Build Array from Permutation #1920
    Runtime: 114 ms, faster than 58.32% of JavaScript online submissions for Build Array from Permutation.
    Memory Usage: 46.6 MB, less than 16.64% of JavaScript online submissions for Build Array from Permutation.
    
    Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
    
    A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).

---
+++ build-array-from-permutation.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var buildArray = function(nums) {
+    
+    var ans = [];
+    
+    for(let i=0; i < nums.length; i++) {
+        
+        ans[i] = nums[nums[i]];
+    }
+    
+    return ans;
+    
+};
